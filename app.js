1fa72be8da9353f1219d7d149022e48263c9892b
MissTatineni <bhargavitatineni97@gmail.com>
2023-11-01 16:22:00 -0500

    modified board.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var sweetsRouter=require('./routes/sweets')
+var sweetsRouter=require('./routes/sweets');
+var boardRouter=require('./routes/board');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sweets', sweetsRouter);
+app.use('/board',boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
